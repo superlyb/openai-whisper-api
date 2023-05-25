50457d5b08432b78e7bcaee9e5bf7133c0351ff1
superlyb <tudouxiong@hotmail.com>
2023-05-25 15:03:37 +0200

    Update txt2speech.jsx

--- components/txt2speech.jsx
+++ components/txt2speech.jsx
@@ -2,8 +2,8 @@ import {SpeechConfig,ResultReason,SpeechSynthesizer} from 'microsoft-cognitivese
 
 
 export default function txt2speech(text){
-    const subscriptionKey = NEXT_PUBLIC_TTS_ACCESS_KEY_ID;
-    const serviceRegion = NEXT_PUBLIC_TTS_REGION; // e.g., "westus"
+    const subscriptionKey = process.env.NEXT_PUBLIC_TTS_ACCESS_KEY_ID;
+    const serviceRegion = process.env.NEXT_PUBLIC_TTS_REGION; // e.g., "westus"
     const speechConfig = SpeechConfig.fromSubscription(subscriptionKey,serviceRegion);
     speechConfig.speechSynthesisLanguage = "zh-CN"; 
    // speechConfig.speechSynthesisVoiceName = "zh-CN-henan-YundengNeural";
@@ -13,11 +13,11 @@ export default function txt2speech(text){
     const timeTagRegex = /\d{2}:\d{2}:\d{2}\.\d{3}\s-->\s\d{2}:\d{2}:\d{2}\.\d{3}/g;
 
     const results = text.data.replace(timeTagRegex, '').replace(/^WEBVTT\s*/i, '');
-    const speechSynthesisVoiceName = NEXT_PUBLIC_SPEECHNAME;
+    const speechSynthesisVoiceName = process.env.NEXT_PUBLIC_SPEECHNAME;
     const ssmlText = `
     <speak version='1.0' xmlns='http://www.w3.org/2001/10/synthesis' xmlns:mstts='https://www.w3.org/2001/mstts' xml:lang='zh-CN'>
       <voice name="${speechSynthesisVoiceName}">
-          <mstts:express-as style='${NEXT_PUBLIC_SPEECHSTYLE}' styledegree='2'>
+          <mstts:express-as style='${process.env.NEXT_PUBLIC_SPEECHSTYLE}' styledegree='2'>
               ${results}
           </mstts:express-as>
       </voice>
