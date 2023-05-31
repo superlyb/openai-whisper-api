0b9a071e4251e55a8f5744720095eac380d47afe
superlyb <tudouxiong@hotmail.com>
2023-05-31 15:13:19 +0200

    Update route.js

--- app/api/route.js
+++ app/api/route.js
@@ -1,6 +1,6 @@
-import fs from 'fs'
+/* import fs from 'fs'
 import path from 'path'
-import { exec } from 'child_process'
+import { exec } from 'child_process' */
 import axios from 'axios'
 import FormData from 'form-data'
 
@@ -54,8 +54,8 @@ async function upLoadfile2cloud(file,filename){
     }
 
 }
-
-/* function getFileType(filename) {
+/*
+ function getFileType(filename) {
     // Get the file extension
     const extension = filename.split('.').pop();
 
@@ -69,9 +69,9 @@ async function upLoadfile2cloud(file,filename){
     };
 
     return mimeTypes[extension || ''] || 'Unknown file type';
-} */
-
-export async function POST(req,cors) {
+} 
+*/
+ export async function POST(req,cors) {
 
 
 
@@ -83,9 +83,6 @@ export async function POST(req,cors) {
     const raw_options = cleanInput(form.get('options'))
    
 
-    /**
-     * Simple form validation
-     */
     if(!blob || !name || !datetime) {
         return new Response('Bad Request', {
             status: 400,
@@ -97,104 +94,9 @@ export async function POST(req,cors) {
 
     const buffer = Buffer.from( await blob.arrayBuffer() )
     const filename = `${name}`
-    //await bufferToFile(buffer, filename);
-    // upLoadfile2cloud(buffer,filename)
-    //upload to R2
-
- //  let filepath = `${path.join('public', 'uploads', filename)}`
-    
- //   fs.writeFileSync(filepath, buffer)
-
-    /**
-     * We are going to check the file size here to decide
-     * whether to send it or not to the API.
-     * As for the min file size value, it is based on my testing.
-     * There is probably a better way to check if the file has no audio data.
-     */
-/*     const minFileSize = 18000 // bytes
-    const stats = fs.statSync(filepath)
-
-    if(parseInt(stats.size) < minFileSize) {
-
-        return new Response('Bad Request', {
-            status: 400,
-        })
-    }  */
-
-    const flagDoNotUseApi = process.env?.DO_NOT_USE_API === 'true'
  
- /*    if(flagDoNotUseApi) {
-        
-        const outputDir = path.join('public', 'uploads') 
-
-        let sCommand = `whisper './${filename}' --language ${options.language} --temperature ${options.temperature} --model tiny --output_dir '${outputDir}'`
-        if(options.endpoint === 'translations') {
-            sCommand = `whisper './${filename}' --language ${options.language} --task translate --temperature ${options.temperature} --model tiny --output_dir '${outputDir}'`
-        }
-
-        const retval = await new Promise((resolve, reject) => {
-
-            exec(sCommand, (error, stdout, stderr) => {
-                
-                if (error) {
-                    
-                    resolve({
-                        status: 'error',
-                        message: "Failed to transcribe [1]",
-                    })
-    
-                } else {
-    
-                    resolve({
-                        status: 'ok',
-                        error: stderr,
-                        //pid: getSimpleId(),
-                        out: stdout,
-                        //url: fileUrl,
-                        //datetime: dateTimeCreated,
-                    })
-    
-                }
-                
-            })
-    
-        })
-
-        if(retval.status === "error" || retval.out.length === 0) {
-            return new Response('Bad Request', {
-                status: 400,
-            })
-        }
-
-        /**
-         * retval.out format: '[00:01.000 --> 00:02.000]  thank\n' +
-         *             '[00:02.720 --> 00:03.720]  you\n' +
-         *
-         *
-        let sout = []
-        let stokens = retval.out.split('\n')
-        for(let i = 0; i < stokens.length; i++) {
-            let n = stokens[i].indexOf(']')
-            if(n > 0) {
-                let s1 = stokens[i].substr(0, n + 1)
-                let s2 = stokens[i].substr(n + 1)
-                sout.push(s1)
-                sout.push(s2)
-            } else {
-                sout.push(stokens[i])
-            }
-        }
-
-        return new Response(JSON.stringify({ 
-            datetime,
-            filename,
-            data: sout.join('\n'),
-        }), {
-            status: 200,
-        })
-
-    }   */
-     
+    const flagDoNotUseApi = process.env?.DO_NOT_USE_API === 'true'
+   
     let header = {
         'Content-Type': 'multipart/form-data',
         'Accept': 'application/json',
@@ -237,32 +139,6 @@ export async function POST(req,cors) {
         //console.log("response"+process.env.NEXT_PUBLIC_OPENAI_APIKEY)
         const data = response.data
 
-        /**
-         * Sample output
-         */
-        //const data = "WEBVTT\n\n00:00:00.000 --> 00:00:04.000\nThe party is starting now hurry up, let's go.\n00:00:04.000 --> 00:00:07.000\nHold this one, okay, do not drop it."
-/*          */ 
-/*         const prompttext =response.data
-        const configuration = new Configuration({
-            apiKey: process.env.NEXT_PUBLIC_OPENAI_APIKEY, // replace with your own OpenAI API key
-          });
-          
-        const openai = new OpenAIApi(configuration);
-          
-        const gptResponse = await openai.createCompletion({
-            model:"gpt-3.5-turbo",
-            messages: [
-                {
-                  role: "system",
-                  content: "You are a helpful assistant. if words lost, complete the sentenace ,"
-                },
-                {
-                  role: "user",
-                  content: "今天天气如何"
-                }
-              ],
-   
-        }); */
         const origin = req.headers.origin;
         // Return CORS headers for preflight request
 
@@ -330,6 +206,7 @@ export async function GET() {
 }
 export async function OPTIONS(req) {
     try {
+        //console.log("re2q",req)
         const origin = req.headers.origin;
         // Return CORS headers for preflight request
 
@@ -353,4 +230,4 @@ export async function OPTIONS(req) {
                 status: 500,
             })
       }
-}
+} 
